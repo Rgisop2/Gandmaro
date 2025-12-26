b2c89c82ddb8c12996205a2f38c6e1eca654936d
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-12-26 14:48:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "38627319"))
+API_HASH = environ.get("API_HASH", "18b0827896e979267ae2251b63830827")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8298659033:AAGnS9bKVidzKOISev_Ek9GCQqFM0NykCkQ")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001918476761"))
+ADMINS = int(environ.get("ADMINS", "1327021082"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://poulomig644_db_user:d9MMUd5PsTP5MDFf@cluster0.q5evcku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
