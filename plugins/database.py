a824ff35bf19c46761ba88b87943cd1bc7b761bd
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-12-26 15:05:59 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -67,5 +67,18 @@ class Database:
         """Get user's last link request timestamp"""
         user = await self.col.find_one({'id': int(user_id)})
         return user.get('last_link_request', 0) if user else 0
+    
+    async def set_relay_user_session(self, session_string, admin_id):
+        """Store relay user account session string"""
+        await self.relay_col.update_one(
+            {'_id': 'relay_session'},
+            {'$set': {'session': session_string, 'set_by': admin_id, 'timestamp': time.time()}},
+            upsert=True
+        )
+    
+    async def get_relay_user_session(self):
+        """Retrieve relay user account session string"""
+        config = await self.relay_col.find_one({'_id': 'relay_session'})
+        return config.get('session') if config else None
 
 db = Database(DB_URI, DB_NAME)
